e416a854f0496d5a1427057c165595548efd1a2b
Tom Steele <thomasjsteele@gmail.com>
2015-07-24 16:25:22 -0700

    Adds comment to AuthInterface

--- auth_interface.go
+++ auth_interface.go
@@ -1,5 +1,8 @@
 package lair
 
+// AuthInterface is a resource which exposes some functionality
+// and is protected by some form of authentication. For example
+// a Cisco SSL VPN.
 type AuthInterface struct {
 	ID            string `json:"_id" bson:"_id"`
 	ProjectID     string `json:"projectId" bson:"projectId"`
