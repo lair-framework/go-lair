fd990b61e34777b1658a5a6f9ebba947d6850ecc
Tom Steele <thomasjsteele@gmail.com>
2015-08-09 22:06:36 -0700

    Adds address and state to netblock

--- netblock.go
+++ netblock.go
@@ -14,7 +14,9 @@ type Netblock struct {
 	MiscEmails     string `json:"miscEmails" bson:"miscEmails"`
 	TechEmails     string `json:"techEmails" bson:"techEmails"`
 	Name           string `json:"name" bson:"name"`
+	Address        string `json:"address" bson:"address"`
 	City           string `json:"city" bson:"city"`
+	State          string `json:"state" bson:"state"`
 	Country        string `json:"country" bson:"country"`
 	PostalCode     string `json:"postalCode" bson:"postalCode"`
 	Created        string `json:"created" bson:"created"`
